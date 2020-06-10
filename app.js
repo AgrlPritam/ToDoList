a8c9812e02765f60dfec47dadf7bc7a7364256e3
Pritam Pratik Agrawal <pritamagrawal95@gmail.com>
2020-06-10 17:31:11 +0530

    Added ports for Heroku Deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://admin-pritam:Tcs%402020%23@cluster0-shard-00-00-ctked.mongodb.net:27017,cluster0-shard-00-01-ctked.mongodb.net:27017,cluster0-shard-00-02-ctked.mongodb.net:27017/todolistDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true});
+mongoose.connect("mongodb://admin-pritam:<password>@cluster0-shard-00-00-ctked.mongodb.net:27017,cluster0-shard-00-01-ctked.mongodb.net:27017,cluster0-shard-00-02-ctked.mongodb.net:27017/todolistDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true});  //Edit Password for MongoDB here
 const itemsSchema = mongoose.Schema({
     name: {
         type: String,
